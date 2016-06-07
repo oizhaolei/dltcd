c63680b0e7d679e24114380f70615212fcf66086
oizhaolei <oizhaolei@gmail.com>
2016-06-07 16:05:58 +0800

    clean resource

--- fukushima/contact.php
+++
@@ -1,7 +0,0 @@
-<br />
-<b>Warning</b>:  reset() [<a href='function.reset'>function.reset</a>]: Passed variable is not an array or object in <b>/home/tpbizbrk/public_html/template/site_flash/contact.php</b> on line <b>15</b><br />
-<br />
-<b>Warning</b>:  current() [<a href='function.current'>function.current</a>]: Passed variable is not an array or object in <b>/home/tpbizbrk/public_html/template/site_flash/contact.php</b> on line <b>20</b><br />
-Your message was successfully sent!<script>
-	resizeTo(300, 300);
-</script>
